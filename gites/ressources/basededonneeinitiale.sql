8a269695b2f3bc033ff43b70ad420016b5dc17cd
JonCourbon <joncourbon@gmail.com>
2019-10-03 22:06:10 +0200

    Ajout base de données initiale (table gites basique)

---
+++ gites/ressources/basededonneeinitiale.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Jeu 03 Octobre 2019 à 22:02
+-- Version du serveur :  5.7.27-0ubuntu0.18.04.1
+-- Version de PHP :  7.2.19-0ubuntu0.18.04.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gites`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gites`
+--
+
+CREATE TABLE `gites` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `departement` varchar(100) DEFAULT NULL,
+  `urlphoto` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `gites`
+--
+ALTER TABLE `gites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `gites`
+--
+ALTER TABLE `gites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
